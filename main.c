78e632ea7da5553455411e69567242088daddcd2
dla1934-byte <dla1934@sookmyung.ac.kr>
2025-11-16 19:56:59 +0900

    11_5

--- main.c
+++ main.c
@@ -5,23 +5,17 @@
 
 int main(int argc, char *argv[])
 {
-   int i;
-   int grade[5];
-   int sum=0;
-   
-   for (i=0;i<5;i++)
-   {
-     printf("grade[%i] = ", i);
-     scanf("%d", &grade[i]);
-   } 
-   
-   for (i=0;i<5;i++)
-   {
-       printf("grade[%i]=%i\n", i, *(grade+i));
-       sum = sum + *(grade+i);
-   }
-   
-   printf("Average : %i\n", sum/5);
+   char *pc;
+   int *pi;
+   double *pd;
+   pc = (char *)10000;
+   pi = (int*)10000;
+   pd = (double *)10000;
+   printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+   pc++;
+   pi++;
+   pd++;
+   printf("증가 후 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
   
   system("PAUSE");	
   return 0;
